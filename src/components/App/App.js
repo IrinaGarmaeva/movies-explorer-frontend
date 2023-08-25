8d1a3f772e050d8d2d277de890b06b90ee580227
Irina Garmaeva <garmaeva-i@mail.ru>
2023-08-25 18:35:23 +0300

    Feature: add Stack component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,7 @@
 import Header from "../Header/Header";
 import Hero from "../Hero/Hero";
 import About from "../About/About";
+import Stack from "../Stack/Stack";
 import Footer from '../Footer/Footer'
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
           <main>
             <Hero />
             <About />
+            <Stack />
           </main>
           <Footer />
         </div>
