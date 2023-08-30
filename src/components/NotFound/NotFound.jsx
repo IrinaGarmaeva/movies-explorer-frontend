7d21fe3706dd3f251b33b46f7fe4bc28eb818271
Irina Garmaeva <garmaeva-i@mail.ru>
2023-08-30 21:38:16 +0200

    Feature: add NotFound component, make responsive

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import { Link } from "react-router-dom";
+import './NotFound.css';
 
 const NotFound = () => {
   return (
-    <div>NotFound</div>
+    <div className="notfound">
+      <div className="notfound__block">
+        <h2 className="notfound__title">404</h2>
+        <p className="notfound__subtitle">Страница не найдена</p>
+        <Link to='/' className="notfound__button">Назад</Link>
+      </div>
+    </div>
   )
 }
 
