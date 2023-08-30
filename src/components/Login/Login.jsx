faf32a468ddd26ee294a1e2c4192e4ce76cc813d
Irina Garmaeva <garmaeva-i@mail.ru>
2023-08-30 13:46:01 +0200

    Feature: add Login component, make it responsive

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,9 +1,57 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import useFormAndValidation from "../../hooks/useFormAndValidation";
+import logo from '../../images/logo.svg'
 import './Login.css'
 
 const Login = () => {
+    const [isLoading, setIsLoading] = useState(false);
+    const { values, errors, handleChange, resetForm } = useFormAndValidation();
+
+    function handleSubmit (evt) {
+      evt.preventDefault();
+      const { email, password } = values;
+      console.log("You sucessfully logged in")
+    }
+
   return (
-    <div>Login</div>
-  )
+    <div className="login">
+      <img src={logo} alt="Логотип" className="login__logo"/>
+      <h2 className="login__title">Рады видеть!</h2>
+      <form className="login__form" onSubmit={handleSubmit}>
+        <fieldset className="login__fieldset">
+          <label htmlFor="email" className="login__label">E-mail</label>
+          <input
+            value={values.email || ''}
+            onChange={handleChange}
+            name="email"
+            type="email"
+            className="login__input"
+            // placeholder="Email"
+            required
+          ></input>
+          <label htmlFor="password" className="login__label">Пароль</label>
+          <input
+            value={values.password || ''}
+            onChange={handleChange}
+            name="password"
+            type="password"
+            className="login__input"
+            // placeholder="Пароль"
+            required
+          ></input>
+        </fieldset>
+        <button className="login__button">
+          {isLoading ? "Вход ... " : "Войти"}
+        </button>
+      </form>
+      <div className="login__register">
+        <p className="login__register-text">Ещё не зарегистрированы?</p>
+        <Link to="/signup" className="login__register-button link">Регистрация</Link>
+      </div>
+    </div>
+  );
+
 }
 
 export default Login
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,55 +1,63 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import './Register.css';
+import useFormAndValidation from "../../hooks/useFormAndValidation";
 import logo from '../../images/logo.svg'
 
 const Register = () => {
   const [isLoading, setIsLoading] = useState(false);
+  const { values, errors, handleChange } = useFormAndValidation();
+
+  function handleSubmit (evt) {
+    evt.preventDefault();
+    const { name, email, password, resetForm } = values;
+    console.log("You sucessfully registered")
+  }
 
   return (
     <div className="register">
       <img src={logo} alt="Логотип" className="register__logo"/>
-      <h2 className="auth__title">Добро пожаловать!</h2>
-      <form className="register__form auth__form">
-        <fieldset className="auth__fieldset">
-          <label htmlFor="name" className="auth__label">Имя</label>
+      <h2 className="register__title">Добро пожаловать!</h2>
+      <form className="register__form" onSubmit={handleSubmit}>
+        <fieldset className="register__fieldset">
+          <label htmlFor="name" className="register__label">Имя</label>
           <input
-            // value={formValue.email}
-            // onChange={handleChange}
+            value={values.name || ''}
+            onChange={handleChange}
             name="name"
             type="name"
-            className="auth__input"
+            className="register__input"
             // placeholder="Введите ваше имя"
             required
           ></input>
-          <label htmlFor="email" className="auth__label">E-mail</label>
+          <label htmlFor="email" className="register__label">E-mail</label>
           <input
-            // value={formValue.email}
-            // onChange={handleChange}
+            value={values.email || ''}
+            onChange={handleChange}
             name="email"
             type="email"
-            className="auth__input"
+            className="register__input"
             // placeholder="Email"
             required
           ></input>
-          <label htmlFor="password" className="auth__label">Пароль</label>
+          <label htmlFor="password" className="register__label">Пароль</label>
           <input
-            // value={formValue.password}
-            // onChange={handleChange}
+            value={values.password || ''}
+            onChange={handleChange}
             name="password"
             type="password"
-            className="auth__input"
+            className="register__input"
             // placeholder="Пароль"
             required
           ></input>
         </fieldset>
-        <button className="auth__button">
+        <button className="register__button">
           {isLoading ? "Регистрация ... " : "Зарегистрироваться"}
         </button>
       </form>
-      <div className="auth__login">
-        <p className="auth__login-text">Уже зарегистрированы?</p>
-        <Link to="/signin" className="auth__login-button">Войти</Link>
+      <div className="register__login">
+        <p className="register__login-text">Уже зарегистрированы?</p>
+        <Link to="/signin" className="register__login-button link">Войти</Link>
       </div>
     </div>
   );
